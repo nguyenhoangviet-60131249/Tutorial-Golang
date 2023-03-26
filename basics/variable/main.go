7baa90aef8c53a63092d7e1856386665fbe25634
hoangviet3236 <hoangviet3236@gmail.com>
2023-03-26 12:38:20 +0700

    update

---
+++ basics/variable/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// define variable i with type String
+	var str = "Hello Golang"
+	fmt.Println(str)
+	// define variable num with type Number
+	var num = 10
+	// or var num int
+	// num =10
+	fmt.Println(num)
+}
